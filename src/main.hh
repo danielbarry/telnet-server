e4029c9405addf7e0e573ece9650e7941c2b6a43
Dan <danbarry16@googlemail.com>
2016-04-02 13:47:50 +0100

    Main header file

---
+++ src/main.hh
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+/**
+ * Main.hh
+ *
+ * The main class that parses the parameters the program runs with.
+ **/
+class Main{
+  /**
+   * error()
+   *
+   * Writes an error message to the terminal.
+   *
+   * @param msg The message to be displayed.
+   **/
+  static void error(std::string msg);
+};
