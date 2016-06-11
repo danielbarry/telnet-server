0619e822ec1081af0ff74378ad753ccc483c5d12
Dan <danbarry16@googlemail.com>
2016-06-11 11:50:26 +0100

    Define how server will be run

---
+++ src/server.hh
@@ -0,0 +1,40 @@
+#include <netinet/in.h>
+
+/**
+ * Server()
+ *
+ * Handles the overall server system.
+ **/
+class Server{
+  public:
+    int serverPort;
+    long serverTimeout;
+    int sockfd;
+
+    /**
+     * Server()
+     *
+     * Initialises the Server object.
+     *
+     * @param port The port to initialise the Server object on.
+     * @param timeout The default timeout to wait for a response and/or action
+     * from the client machine. This amount is measured in milliseconds.
+     **/
+    Server(int port, long timeout);
+
+    /**
+     * ~Server()
+     *
+     * The Server's deconstruction, allowing it to be safely closed down.
+     **/
+    ~Server();
+
+    /**
+     * loop()
+     *
+     * The main blocking loop of the Server. Due to design, the only reason
+     * this is likely to ever exit is through an error which is very unlikely.
+     * Therefore this loop is likely to run until manually closed.
+     **/
+    void loop();
+};
