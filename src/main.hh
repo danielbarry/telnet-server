183af6c7e270fd735d8371e66eadaee033fc2d84
Dan <danbarry16@googlemail.com>
2016-06-11 17:07:59 +0100

    Disallow double declaration of Main header

--- src/main.hh
+++ src/main.hh
@@ -1,19 +1,23 @@
-#include <iostream>
-#include <string>
+#ifndef MAIN_HH
+  #define MAIN_HH
 
-/**
- * Main.hh
- *
- * The main class that parses the parameters the program runs with.
- **/
-class Main{
-  public:
-    /**
-     * error()
-     *
-     * Writes an error message to the terminal.
-     *
-     * @param msg The message to be displayed.
-     **/
-    static void error(std::string msg);
-};
+  #include <iostream>
+  #include <string>
+
+  /**
+   * Main.hh
+   *
+   * The main class that parses the parameters the program runs with.
+   **/
+  class Main{
+    public:
+      /**
+       * error()
+       *
+       * Writes an error message to the terminal.
+       *
+       * @param msg The message to be displayed.
+       **/
+      static void error(std::string msg);
+  };
+#endif
