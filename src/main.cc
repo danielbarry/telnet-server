25808aa93e468465787dde0572a26bd34c97d7bb
Dan <danbarry16@googlemail.com>
2016-06-11 11:51:05 +0100

    Initialise Server object

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,15 @@
 #include "main.hh"
 
+#include "server.cc"
+
 int main(int argc, char** argv){
   /* Default values */
   int port = 3333;
   long timeout = 2000;
+  /* TODO: Accept command line arguments. */
+  /* Create a new Server object and run it */
+  Server* server = new Server(port, timeout);
+  server->loop();
   return 0;
 }
 
