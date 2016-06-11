a1bf4c973b74bfec431e2ad433b48d42baa0e4b4
Dan <danbarry16@googlemail.com>
2016-06-11 17:07:39 +0100

    Disallow double declarations

--- src/config.hh
+++ src/config.hh
@@ -1,10 +1,14 @@
-/**
- * config.hh
- *
- * The configuration file for the server's internal defaults compiled into the
- * system.
- **/
-#define DEFAULT_PORT        23
-#define DEFAULT_TIMEOUT     2000
-#define DEFAULT_BUFFER_SIZE 256
-#define DEFAULT_BUFFER_READ (DEFAULT_BUFFER_SIZE - 1)
+#ifndef CONFIG_HH
+  #define CONFIG_HH
+  /**
+   * config.hh
+   *
+   * The configuration file for the server's internal defaults compiled into the
+   * system.
+   **/
+  #define DEFAULT_PORT        23
+//  #define DEFAULT_PORT        3333
+  #define DEFAULT_TIMEOUT     2000
+  #define DEFAULT_BUFFER_SIZE 256
+  #define DEFAULT_BUFFER_READ (DEFAULT_BUFFER_SIZE - 1)
+#endif
