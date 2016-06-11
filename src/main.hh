02e22ec8b7a71073c1e960d9edba665a5d972297
Dan <danbarry16@googlemail.com>
2016-06-11 11:49:32 +0100

    Method should be declared public

--- src/main.hh
+++ src/main.hh
@@ -7,12 +7,13 @@
  * The main class that parses the parameters the program runs with.
  **/
 class Main{
-  /**
-   * error()
-   *
-   * Writes an error message to the terminal.
-   *
-   * @param msg The message to be displayed.
-   **/
-  static void error(std::string msg);
+  public:
+    /**
+     * error()
+     *
+     * Writes an error message to the terminal.
+     *
+     * @param msg The message to be displayed.
+     **/
+    static void error(std::string msg);
 };
