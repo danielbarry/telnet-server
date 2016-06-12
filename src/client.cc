d8b5d39cc9e600282015ade374ea290f68e5e71b
Dan <danbarry16@googlemail.com>
2016-06-12 06:46:38 +0100

    Remove debug statements

--- src/client.cc
+++ src/client.cc
@@ -4,28 +4,22 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#include<stdio.h> // TODO: Remove me
-
 int* Client::serverSockfd;
 socklen_t Client::clilen;
 
 void Client::init(int* sockfd){
   serverSockfd = sockfd;
-  printf("server orig->%i\n", *sockfd); // TODO: Remove me
   clilen = sizeof(cli_addr);
 }
 
 Client::Client(){
   clientSocketid = accept(*serverSockfd, (struct sockaddr*)&cli_addr, &clilen);
-  printf("server after->%i\n", *serverSockfd); // TODO: Remove me
-  printf("client origin->%i\n", clientSocketid); // TODO: Remove me
 }
 
 Client::~Client(){
 }
 
 void* Client::run(){
-  printf("client after->%i\n", clientSocketid); // TODO: Remove me
   /* Make sure we were able to bind to the client */
   if(clientSocketid < 0){
     Main::error("Failed to bind to client.");
